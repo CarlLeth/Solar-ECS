c71ac883f52b8aa43e91bde16b35c5baec6ad666
Carl Leth <>
2022-11-14 17:53:41 -0700

    Modernized implementation of ListSystem to not rely on (old, removed) IReductionStrategies. SolarEcs -> 2.1.0; SolarEcs.Common -> 2.0.1.

---
+++ src/SolarEcs.Common/Lists/DualListSystem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SolarEcs.Common.Lists
+{
+    /// <summary>
+    /// Implementation of IListSystem that splits membership models into different components based on whether they're ordered or not.
+    /// </summary>
+    public class DualListSystem : IListSystem
+    {
+        readonly IStore<OrderedListMembership> OrderedMembers;
+        readonly IStore<UnorderedListMembership> UnorderedMembers;
+
+        public DualListSystem(IStore<OrderedListMembership> orderedMembers, IStore<UnorderedListMembership> unorderedMembers)
+        {
+            OrderedMembers = orderedMembers;
+            UnorderedMembers = unorderedMembers;
+        }
+
+        public IQueryPlan<ListMembershipModel> Query
+        {
+            get
+            {
+                var unorderedQuery = UnorderedMembers.ToQueryPlan()
+                    .Select(o => new ListMembershipModel(o.Entity, o.List, null));
+
+                var orderedQuery = OrderedMembers.ToQueryPlan()
+                    .Select(o => new ListMembershipModel(o.Entity, o.List, o.Ordinal));
+
+                return unorderedQuery.Concat(orderedQuery);
+            }
+        }
+
+        public IRecipe<ListMembershipModel> Recipe
+        {
+            get
+            {
+                return Query.StartRecipe()
+                    .IncludeSimple(UnorderedMembers.ToRecipe(), model => new UnorderedListMembership(model.Entity, model.List), model => model.Ordinal == null)
+                    .IncludeSimple(OrderedMembers.ToRecipe(), model => new OrderedListMembership(model.Entity, model.List, model.Ordinal.Value), model => model.Ordinal.HasValue);
+            }
+        }
+    }
+}
